6467c4240979bec745d2e9f40d6992daa3fa27af
Ladalo-Daniel <balaladalo@gmail.com>
2023-12-30 03:38:23 +0100

    added quiz feature a bit

--- src/lib/store/questions.ts
+++ src/lib/store/questions.ts
@@ -1,19 +1,7 @@
-import { useSearchParams } from 'next/navigation'
 import { create } from 'zustand'
-import { useFetchRandomQuestions } from '../react-query'
 
-export const useStore = (userId: string, courseId: string) => create((set) => {
-    const searchParams = useSearchParams()
-    const noq = searchParams.get("noq")
-
-    const { data: quizQuestions, isPending } = useFetchRandomQuestions({
-        user_id: userId,
-        course_id: parseInt(courseId),
-        numberOfQuestions: parseInt(noq!)
-    })
-    return {
-        questions: quizQuestions,
-        increasePopulation: () => set((state: any) => ({ bears: state.bears + 1 })),
-        removeAllBears: () => set({ bears: 0 }),
-    }
-})
+const useStore = create((set) => ({
+  bears: 0,
+  increasePopulation: () => set((state: any) => ({ bears: state.bears + 1 })),
+  removeAllBears: () => set({ bears: 0 }),
+}))
