4fcd6b1aaa0aaaa9dd21195471fc0242d1774192
Ladalo-Daniel <balaladalo@gmail.com>
2024-01-01 08:39:50 +0100

    new commit

--- src/app/dashboard/s/quiz/history/NavigateCourses.tsx
+++ src/app/dashboard/s/quiz/history/NavigateCourses.tsx
@@ -6,13 +6,6 @@ import React, { useState } from 'react'
 
 const NavigateCourses = ({ courseIds }: { courseIds: number[] }) => {
     const [courseNames, setCourseNames] = useState<string[]>([])
-    courseIds.forEach(id => {
-        const { data } = useGetCourseByQuery({
-            column: "id",
-            row: id
-        })
-        setCourseNames(prev => [...prev, data?.data.at(0)?.name!])
-    })
   return (
     <div>
         {
