af378a4e3a7b3474eb1fb9b827ab9e2bcdf5e23c
Ladalo-Daniel <balaladalo@gmail.com>
2024-01-02 10:17:20 +0100

    newest commit

--- src/app/dashboard/s/ai/page.tsx
+++ src/app/dashboard/s/ai/page.tsx
@@ -1,13 +1,17 @@
-import MaxWrapper from '@/components/MaxWrapper'
-import { Alert } from '@/components/ui/alert'
 import React from 'react'
+import MaxWrapper from '@/components/MaxWrapper'
+import { Alert, AlertDescription } from '@/components/ui/alert'
+import { SparklesIcon } from 'lucide-react'
 
 const AIPage = () => {
   return (
     <MaxWrapper className='bg-background flex-1'>
         <h2 className="text-2xl py-2 text-primary">Welcome to our AI guide.</h2>
-        <Alert>
+        <Alert className='flex gap-2'>
+            <SparklesIcon />
+            <AlertDescription>
             Note that this feature is a work in progress... please check back soon as the @Naijaschools team is working to ensure it is served up to you. 
+            </AlertDescription>
         </Alert>
     </MaxWrapper>
   )
