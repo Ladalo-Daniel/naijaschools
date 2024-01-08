7cbd10e93fd61ffe06d64dc3ae53631fb51d4e0b
Ladalo-Daniel <balaladalo@gmail.com>
2024-01-08 22:59:26 +0100

    new commit

--- src/app/dashboard/posts/PostCard.tsx
+++ src/app/dashboard/posts/PostCard.tsx
@@ -12,6 +12,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import PostStats from './PostStats'
+import { Skeleton } from '@/components/ui/skeleton'
 
 /**
  * A reply is still a post
@@ -23,17 +24,33 @@ const PostCard = ({ post, isReply }: { post: Post, isReply?: boolean }) => {
   return (
     <Card className={`p-4 border-muted bg-background rounded-3xl ${isReply ? "border-none": "border"} lg:p-7 w-full max-w-screen-sm hover:transition-all flex flex-row gap-1.5`}>
       <div className="w-12">
-        <Avatar src={author?.data?.image_url || '/icons/profile-placeholder.svg'}/>
-        {<Separator orientation='vertical' className={`h-[75%] mx-auto mt-1 ${ isReply ? "text-muted" : "" } mb-8`}/>}
+        {
+          isPending ? <Skeleton className='w-12 h-12 rounded-full' /> : (
+            <Avatar src={author?.data?.image_url || '/icons/profile-placeholder.svg'}/>
+          ) 
+        }
+        <Separator orientation='vertical' className={`h-[75%] mx-auto mt-1 ${ isReply ? "text-muted" : "" } mb-8`}/>
       </div>
       <div className="flex flex-col gap-0.5 p-2 md:p-3 lg:p-4 -mt-8 w-full pb-4">
         <CardHeader className='flex items-center gap-1 w-full px-0'>
           <div className='w-full md:flex-row md:flex-wrap flex md:items-center gap-1 flex-col'>
-            <h2 className='text-muted-foreground my-1'>{author?.data?.first_name} {author?.data?.last_name}</h2>
-            <span className="hidden md:block">.</span>
-            <p className='text-primary tracking-tighter'>@{author?.data?.username}</p>
-            <span className="hidden md:block">.</span>
-            <p className="text-muted">{shortMultiFormatDateString(post.created_at)}</p>
+            {
+              isPending ? (
+                <>
+                <Skeleton className='h-5 w-28' />
+                <Skeleton className='h-5 w-16' />
+                <Skeleton className='h-5 w-24' />
+                </>
+              ) : (
+                <>
+                <h2 className='text-muted-foreground my-1'>{author?.data?.first_name} {author?.data?.last_name}</h2>
+                <span className="hidden md:block">.</span>
+                <p className='text-primary tracking-tighter'>@{author?.data?.username}</p>
+                <span className="hidden md:block">.</span>
+                <p className="text-muted">{shortMultiFormatDateString(post.created_at)}</p>
+                </>
+              )
+            }
           </div>
         </CardHeader>
         <CardBody as={Link} href={`/dashboard/${author?.data?.username}/${post.id}`} className="flex flex-col px-0">
