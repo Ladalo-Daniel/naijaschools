6d0657839176d8a11261d63c7673540d0ff40e04
Ladalo-Daniel <balaladalo@gmail.com>
2024-01-13 21:51:08 +0100

    new commit

--- src/openai/index.ts
+++ src/openai/index.ts
@@ -31,3 +31,22 @@ export async function getAISearchResponse(query: string) {
 
   return chatCompletion.choices[0].message.content
 }
+
+export async function getAIChatResponse(query: string) {
+  const stream = await openai.chat.completions.create({
+    messages: [
+        { role: 'system', content: "You are an enthusiastic Naijaschools AI chat assistant who loves to help people with their questions on the Naijaschools platform - an educational platform." }, 
+        { role: 'assistant', content: "You are to help this user with the best response to his chat. Be as cheerful as you can be. If you can't find an answer to the user's specific chats, simply reply with: 'Sorry, I cannot help with that'." }, 
+        { role: 'user', content: query }
+],
+    model: 'gpt-3.5-turbo',
+    stream: true,
+  })
+
+  for await (const part of stream) {
+    process.stdout.write(part.choices[0]?.delta?.content || '');
+    return part.choices[0]?.delta?.content || ''
+  }
+  // return chatCompletion.toReadableStream().getReader()
+}
+
